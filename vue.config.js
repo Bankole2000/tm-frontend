86ed323b3e01204d3e0c326b37938579df9cd231
Bankole <ebankole@aidml.com>
2023-07-23 01:37:23 +0100

    test package sync

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ], 
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "TimiKeys";
+      return args;
+    });
+  },  
+  publicPath: process.env.NODE_ENV === "production" ? "/tm-frontend/" : "/"
+}
